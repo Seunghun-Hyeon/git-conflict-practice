ab90cb195c8870dcf26ad487c69710eeab2de56c
현승훈 <tmdgns4970@handong.ac.kr>
2025-05-21 22:56:11 +0900

    자 드가자

---
+++ main/main/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  main
+//
+//  Created by 현승훈 on 5/21/25.
+//
+
+import SwiftUI
+struct ContentView: View {
+var body: some View {
+    
+    Text("안녕하세요")
+        .font(.system(size: 24))
+        .multilineTextAlignment(.center)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+}
+}
---
+++ main/main/mainApp.swift
@@ -0,0 +1,17 @@
+//
+//  mainApp.swift
+//  main
+//
+//  Created by 현승훈 on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct mainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
